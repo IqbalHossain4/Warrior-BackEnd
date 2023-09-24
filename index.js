a5aa1555b738044b7f01e4554cb6e665f6158777
iqbalhossain4 <iqbal-hossain@mail.com>
2023-09-24 09:33:20 +0600

    delete user

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 
@@ -63,6 +65,14 @@ async function run() {
     // get User
     app.get("/user", async (req, res) => {
       const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    //Delete User
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
       res.send(result);
     });
 
